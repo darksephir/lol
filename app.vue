cd448b3380b4fc11b33805283daaa20f6cf72bc4
darksephir <57842349+darksephir@users.noreply.github.com>
2023-08-19 15:45:12 -0500

    Esto es una prueba

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+    <NuxtLayout>
+        <NuxtPage/>
+    </NuxtLayout>
+</template>
+<script setup lang="ts">
+const nuxtApp = useNuxtApp();
+const loading = ref(false);
+
+nuxtApp.hook("page:start", () => {
+    loading.value = true;
+});
+
+nuxtApp.hook("page:finish", () => {
+    loading.value = false;
+});
+</script>
